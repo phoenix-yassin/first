425021dbf871de1b1ea0543ea933b4147e513347
易云辉 <yiyunhui@cyou-inc.com>
2017-02-14 18:18:43 +0800

    jquery 源码解读--2&3

--- js/test.js
+++ js/test.js
@@ -1,10 +1,8 @@
 (function(window, undefined) {
   'use strict';
-
-
-
-
-
+/**
+ * balabalaXXXX
+ */
   window.jQuery = window.$ = jQuery;
 })(window);
 
@@ -72,9 +70,8 @@ $.map = function(elems, cb, arg) {
 }
 
 /**
- * [globalEval description]
- * @param  {[type]} data [description]
- * @return {[type]}      [description]
+ * 此处用到了一种技巧确定函数执行的上下文
+ * 匿名函数+call,用匿名函数传递参数,用call改变上下文和接收参数
  */
 $.globalEval = function(data){
   if(data && jQuery.trim(data)){
@@ -82,4 +79,67 @@ $.globalEval = function(data){
       window['eval'].call(window,data)
     })(data);
   }
+}
+
+/**
+ * [nodeName description]
+ * @param  {[type]} elem [description]
+ * @param  {[type]} name [description]
+ * @return {[type]}      [description]
+ */
+$.nodeName = function(elem, name){
+  return elem.nodename && elem.nodename.toLowerCase() === name;
+}
+
+/**
+ *
+ */
+jQuery.extend = jQuery.fn.extend = function () {
+
+  var target = arguments[0] || {},
+      i = 1,
+      length = arguments.length,
+      source, copy, copyIsArray, clone, name, options,
+      deep = false;
+//如果第一个参数是deep的参数, 那么第二个参数是目标,后面的参数是源
+  if(typeof taget === 'boolean'){
+    deep = target;
+    target = arguments[1] || {}
+    i = 2;
+  }
+//如果目标不是对象类型
+  if(typeof target !== 'object' && !jQuery.isFunction(target)){
+    target = {};
+  }
+  if(length === i){
+    target = this;
+    --i;
+  }
+
+  for(; i < length; i++ ){//
+    if( (options = arguments[i])!= null){
+      for(name in options){
+        src = target[name];
+        copy = options[name];
+
+        if(target === copy){
+          continue;
+        }
+
+        if(deep && (jQuery.isPlainObject(copy) || (jQuery.isArray(copy)))){
+          if(copyIsArray){
+            copyIsArray = false;
+            clone = src && jQuery.isArray(src)? src:[];
+          }else{
+            clone = src && jQuery.isPlain(src)? src : {};
+          }
+          target[ name ] = jQuery.extend( deep, clone, copy );
+        }elseif(copy !== undefined){
+          target[name] = copy;
+        }
+      }
+    }
+
+  }
+  return target;
 }
