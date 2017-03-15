6869ec47fe16b7333d6c47b8337a9e1c66c52a15
易云辉 <yiyunhui@cyou-inc.com>
2017-03-15 11:13:36 +0800

    drop2

--- localStorage/traking.js
+++ localStorage/traking.js
@@ -1,1 +1,48 @@
-traking.js
+let listeners = {};
+let listening = false;
+
+
+let change = (e)=>{
+
+  /*if(!e){
+    e = global ? global.event: window.event;
+  }*/
+  if(!e){
+    e = global.event;
+  }
+  var fn = listeners[e.key];
+};
+
+let listen = ()=>{
+  if(global.addEventListener){
+    global.addEventListener('storage', 'change', false);
+  }else if(global.attachEvent){
+    global.attachEvent('onstorage', change);
+  }else{
+    global.onstorage = change;
+  }
+}
+
+let on = (type, fn)=>{
+  if(listeners[type]){
+    listeners[type].push(fn);
+  }else{
+    listeners[type] = [fn];
+  }
+  if(!listening){
+    listening = true;
+    listen();
+  }
+
+}
+
+let off = (type, fn)=>{
+  let fns = listeners[type];
+  if(fns.length > 1){
+    fns.splice(fns.indexOf(fn), 1);
+  }else{
+    listeners[type] = [];
+  }
+}
+
+export {on,off}
